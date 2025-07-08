3755adeec3669b670ce3f2250a89d3a42d3e4401
Кравцов Иван <ivan.an.kravtsov@gazprombank.ru>
2025-07-08 15:44:05 +0300

    dz-1

--- main.go
+++ main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	SUM = "SUM"
+	AVG = "AVG"
+	MED = "MED"
+)
 
 func main() {
-	fmt.Println(123)
+	operation := getOperation()
+	slice := getSlice()
+	fmt.Println(slice)
+	result := calculate(operation, slice)
+
+	fmt.Println(result)
 }
+
+func calculate(operation string, slice []float64) float64 {
+	var result float64
+	switch operation {
+	case SUM:
+		for _, v := range slice {
+			result += v
+		}
+	case AVG:
+		var sum float64
+		for _, v := range slice {
+			sum += v
+		}
+		result = sum / float64(len(slice))
+	case MED:
+		result = slice[len(slice)/2]
+	}
+
+	return result
+}
+
+func getOperation() string {
+	var operation string
+	for {
+		fmt.Println("Введите операцию (SUM/AVG/MED/)")
+		_, err := fmt.Scan(&operation)
+		if err != nil {
+			continue
+		}
+		if operation != SUM && operation != AVG && operation != MED {
+			fmt.Println("Такой операции нет")
+			continue
+		}
+		break
+	}
+	return operation
+}
+
+func getSlice() []float64 {
+	slice := []float64{}
+	fmt.Println("Введите числа через запятую")
+	var str string
+	fmt.Scan(&str)
+	trimmed := strings.TrimSpace(str)
+	splitted := strings.Split(trimmed, ",")
+	for _, s := range splitted {
+		num, _ := strconv.ParseFloat(s, 64)
+		slice = append(slice, num)
+	}
+
+	return slice
+}
+
+/*
+	NOTES
+*/
+/*
+Удалить 3й элемент
+s = append(s[:2], s[3:]...)
+
+*/
